d039c55894211a8bd9bb8882635bb3eed54fc582
SAP <School@Mac.fios-router.home>
2025-05-15 21:24:44 -0400

    Update searchIndex.js to use Eleventy 3.0 compatible collection API

--- my-blog/src/_data/searchIndex.js
+++ my-blog/src/_data/searchIndex.js
@@ -1,13 +1,15 @@
 module.exports = function(collection) {
-  // Get all blog posts
-  let blogposts = collection.getFilteredByTag('post');
+  // Get all blog posts - updated for Eleventy 3.0
+  let blogposts = collection.filter(item => {
+    return item.data.tags && item.data.tags.includes('post');
+  });
   
   // Create the search index
   return blogposts.map(post => {
     return {
       title: post.data.title,
       url: post.url,
-      content: post.templateContent.replace(/<[^>]*>/g, ''), // Strip HTML tags
+      content: post.templateContent?.replace(/<[^>]*>/g, '') || '', // Strip HTML tags with null check
       tags: post.data.tags ? post.data.tags.filter(tag => tag !== 'post') : [], // Filter out the 'post' tag
       date: post.date.toISOString().slice(0, 10)
     };
